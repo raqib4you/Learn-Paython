77c17ae02e0b01f1c502c109eb9bee1943fe8472
raqib4you <raqibulmia121@gmail.com>
2023-02-15 02:47:24 +0600

    first commit

---
+++ class/Data_Type.py
@@ -0,0 +1,15 @@
+
+Topic = "Data Type"
+print(Topic)
+
+First = "5"
+print(First)
+print(type(First))
+
+Second = "10.5"
+print(Second)
+print(type(Second))
+
+result = (float(First)) * (float(Second))
+
+print(result)
