865d53ff5198e8bc61ff396e9f6143e8cf61a8ba
raqib4you <raqibulmia121@gmail.com>
2023-02-15 23:58:04 +0600

    if

---
+++ class2/if.py
@@ -0,0 +1,17 @@
+# he's goin to cox's bazar
+
+print("he's goin to cox's bazar")
+
+print('he\'s goin to cox\'s bazar')
+
+
+Gender = input(" Enter your Gender m/f")
+
+if Gender == "m":
+    print("Thanks You are Male")
+else:
+    print("No you Are not Male")
+
+
+
+
