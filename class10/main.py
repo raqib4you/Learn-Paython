add82c5641acd816d4abe0cf47f715a6a5568bf3
raqib4you <raqibulmia121@gmail.com>
2023-02-19 11:18:54 +0600

    your comment

--- class10/main.py
+++ class10/main.py
@@ -2,6 +2,10 @@
 #first i need to import requests module
 import requests
 
+#for good view joson data
+import pprint
+
+
 #api data url
 url = "https://api.ipify.org/?format=json"
 
@@ -22,3 +26,29 @@ iplocation_url = f"https://ipapi.co/{ip}/json/"
 r = requests.get(iplocation_url)
 data2 = r.json()
 print(data2)
+
+
+
+
+#pixabay api
+
+key = "7528421-e4cf646978f7cb7446adb39c9"
+keyword = "flowers"
+url2 = f"https://pixabay.com/api/?key={key}&q={keyword}&image_type=photo&pretty=true"
+res2 = requests.get(url2)
+data3 = res2.json()
+
+
+#pprint.pprint(data3)
+
+photo = data3["hits"]
+pprint.pprint(photo)
+
+
+image = photo[0]
+pprint.pprint(image)
+
+for main in image:
+    main = image["largeImageURL"]
+    print(main)
+
